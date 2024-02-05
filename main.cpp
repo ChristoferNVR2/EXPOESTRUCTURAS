238c521d14e344e9b6aa166eb6353abcc2a6608b
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-02-05 17:32:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,20 @@
 
 using namespace std;
 
+int sequentialSearch(const int arr[], int size, int ref) {
+    for (int i = 0; i < size; i++) {
+        if (ref == arr[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int binarySearch(const int arr[], int size, int ref) {
 
     int start = 0;
     int end = size-1;
-    int mid = start + (end-start)/2;
+    int mid = start + (end - start)/2;
 
     while(start <= end) {
         if(arr[mid] == ref) {
@@ -24,20 +33,59 @@ int binarySearch(const int arr[], int size, int ref) {
     return -1;
 }
 
+int recursiveSearch(const int arr[], int start, int end, int ref) { // NOLINT(misc-no-recursion)
+
+    int mid = (end + start)/2;
+
+    if (arr[mid] == ref) {
+        return mid;
+    }
+
+    if (start > end) {
+        return -1;
+    }
+
+    if (ref < arr[mid]) {
+        return recursiveSearch(arr, start, mid - 1, ref);
+    } else {
+        return recursiveSearch(arr, mid + 1, end, ref);
+    }
+}
+
 int main () {
 
     int busq = 11;
+
     int MyArray[10] = {1,2,3,4,5,6,7,8,9,10};
+
     for (int i : MyArray) {
         cout << i << " - ";
     }
     cout << endl;
+
+
+    if (sequentialSearch(MyArray, 10, busq) != -1) {
+        cout << "ELEMENTO " << busq << " ENCONTRADO CON EL INDICE " << sequentialSearch(MyArray, 10, busq) << endl;
+    } else {
+        cout << "ELEMENTO " << busq << " NO ENCONTRADO EN EL ARREGLO" << endl;
+    }
 
     if (binarySearch(MyArray, 10, busq) != -1) {
         cout << "ELEMENTO " << busq << " ENCONTRADO CON EL INDICE " << binarySearch(MyArray, 10, busq) << endl;
     } else {
         cout << "ELEMENTO " << busq << " NO ENCONTRADO EN EL ARREGLO" << endl;
     }
+
+    if (recursiveSearch(MyArray,0, 10, busq) != -1) {
+        cout << "ELEMENTO " << busq << " ENCONTRADO CON EL INDICE " << recursiveSearch(MyArray, 0, 10, busq) << endl;
+    } else {
+        cout << "ELEMENTO " << busq << " NO ENCONTRADO EN EL ARREGLO" << endl;
+    }
+
+
+
+
+
 
     DoublyLinkedList MyList = DoublyLinkedList();
 
@@ -46,12 +94,20 @@ int main () {
     }
     MyList.displayList();
 
+    MyList.sequentialSearch(busq);
+
     if (MyList.binarySearch(busq)) {
         cout << "ELEMENTO " << busq << " ENCONTRADO EN LA LISTA" << endl;
     } else {
         cout << "ELEMENTO " << busq << " NO ENCONTRADO EN LA LISTA" << endl;
     }
-    MyList.displayList();
+
+    if (MyList.recursiveSearch(busq)) {
+        cout << "ELEMENTO " << busq << " ENCONTRADO EN LA LISTA" << endl;
+    } else {
+        cout << "ELEMENTO " << busq << " NO ENCONTRADO EN LA LISTA" << endl;
+    }
+
 
     return 0;
 }
